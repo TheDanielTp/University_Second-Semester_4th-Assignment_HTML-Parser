f6140b27b6e9c12b8085ffd01f83f6f365625d71
TheDanielTp <prof.danial4@gmail.com>
2024-04-18 12:11:04 +0330

    Fixed toString & equals functions

--- src/main/java/Country.java
+++ src/main/java/Country.java
@@ -1,43 +1,63 @@
 import java.util.Objects;
 
-public class Country {
-    private String name;
-    private String capital;
-    private int population;
-    private double area;
+public class Country
+{
+    private final String name;
+    private final String capital;
+    private final int    population;
+    private final double area;
 
-    public Country(String name, String capital, int population, double area) {
-        //TODO
+    public Country (String name, String capital, int population, double area)
+    {
+        this.name       = name;
+        this.capital    = capital;
+        this.population = population;
+        this.area       = area;
     }
 
     @Override
-    public String toString() {
-        //TODO
-        return "";
+    public String toString ()
+    {
+        String string = "";
+
+        string += "Name: " + name + "\n";
+        string += "Capital: " + capital + "\n";
+        string += "Population: " + population + "\n";
+        string += "Area: " + area + "\n";
+
+        return string;
     }
 
     @Override
-    public boolean equals(Object o) {
-        Country country = (Country) o;
-        return population == country.population &&
-                Double.compare(country.area, area) == 0 &&
-                Objects.equals(name, country.name) &&
-                Objects.equals(capital, country.capital);
+    public boolean equals (Object object)
+    {
+        if (getClass() != object.getClass())
+        {
+            return false;
+        }
+
+        Country country = (Country) object;
+        return population == country.population && Double.compare (country.area, area) == 0
+                && Objects.equals (name, country.name) && Objects.equals (capital, country.capital);
     }
 
-    public String getName() {
+    public String getName ()
+    {
         return name;
     }
 
-    public String getCapital() {
+    public String getCapital ()
+    {
         return capital;
     }
 
-    public int getPopulation() {
+    public int getPopulation ()
+    {
         return population;
     }
 
-    public double getArea() {
+    public double getArea ()
+    {
         return area;
     }
 }
